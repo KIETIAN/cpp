565e8d4de5f1d00be441eaed6d0124b34a120022
VAIBHAV KUMAR <108072133+KIETIAN@users.noreply.github.com>
2025-09-07 22:19:02 +0530

    Add files via upload

---
+++ largest_3no.cpp
@@ -0,0 +1,32 @@
+// To input three no. and find the largest among them using nested if else.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,num3;
+    cout << "Enter three number:";
+    cin >> num1 >> num2 >> num3;
+    if(num1>num2)
+     {
+        if(num1>num3)
+        {
+            cout << num1<<"is the largest number "<< endl;
+        }
+        else
+        {
+            cout << num3 << "is the largest number"<<endl;
+        }
+    }   
+         else
+        {
+            if(num2>num3)
+            {
+                cout<<num2<<"is the largest numbeer"<<endl;
+            }
+            else
+            {
+                cout<<num3<<"is the largest number"<<endl;
+            }
+        }
+     return 0;
+}
