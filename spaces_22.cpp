a8cec80de294b0786fe6a7b4125b990a23126a92
VAIBHAV KUMAR <108072133+KIETIAN@users.noreply.github.com>
2025-10-08 17:04:22 +0530

    Add files via upload

---
+++ spaces_22.cpp
@@ -0,0 +1,19 @@
+// input a string and replace all spaces with -.
+#include<iostream>
+using namespace std;
+int main()
+{
+   string str;
+    getline(cin, str);  
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') 
+        {
+            str[i] = '-';  
+        }
+    }
+
+    cout << "Modified string: " << str << endl;
+
+    return 0; 
+}
