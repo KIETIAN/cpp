0d93e015e65d0f68e56a1ac3b17bda95f0f0c1e1
VAIBHAV KUMAR <108072133+KIETIAN@users.noreply.github.com>
2025-11-26 11:43:30 +0530

    Add files via upload

--- 2_shape.cpp
+++ 2_shape.cpp
@@ -1,57 +1,41 @@
-// Define a class Shape, derive Circle and Rectangle from it, and calculate area in each derived class.
+//	Define an abstract class Shape with pure virtual function perimeter(). Derive Square and Circle that implement it.
 #include <iostream>
 using namespace std;
 
-// Base class
 class Shape {
 public:
-    virtual void input() = 0;      // Pure virtual function
-    virtual void calculateArea() = 0; // Pure virtual function
+    virtual void perimeter() = 0;
 };
 
-// Derived class Circle
+class Square : public Shape {
+    int side;
+public:
+    Square(int s) { side = s; }
+    void perimeter() override {
+        cout << "Perimeter of Square = " << 4 * side << endl;
+    }
+};
+
 class Circle : public Shape {
-private:
-    double radius;
+    int radius;
 public:
-    void input() {
-        cout << "Enter radius of circle: ";
-        cin >> radius;
-    }
-    void calculateArea() {
-        double area = 3.14159 * radius * radius;
-        cout << "Area of Circle = " << area << endl;
-    }
-};
-
-// Derived class Rectangle
-class Rectangle : public Shape {
-private:
-    double length, breadth;
-public:
-    void input() {
-        cout << "Enter length of rectangle: ";
-        cin >> length;
-        cout << "Enter breadth of rectangle: ";
-        cin >> breadth;
-    }
-    void calculateArea() {
-        double area = length * breadth;
-        cout << "Area of Rectangle = " << area << endl;
+    Circle(int r) { radius = r; }
+    void perimeter() override {
+        cout << "Perimeter of Circle = " << 2 * 3.14 * radius << endl;
     }
 };
 
 int main() {
-    Circle c;
-    Rectangle r;
+    Shape* s;
 
-    cout << "- Circle -" << endl;
-    c.input();
-    c.calculateArea();
+    Square sq(5);
+    Circle c(7);
 
-    cout << "- Rectangle -" << endl;
-    r.input();
-    r.calculateArea();
+    s = &sq;
+    s->perimeter();
+
+    s = &c;
+    s->perimeter();
 
     return 0;
 }
